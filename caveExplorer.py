bbb635ff4304fb2725e4b13353abbad712f3da17
MahmudurLimon <mahmudurlimon76@gmail.com>
2025-08-28 13:12:38 +0600

    player profile creation complete

--- caveExplorer.py
+++ caveExplorer.py
@@ -6,8 +6,12 @@ def cave_file_func():
     player_name=str(input("Ener Your Name: "))
     player_name=player_name.lower()
     cur_dir=os.getcwd()
-    src_dir=f"src/player/"
+    src_dir=f"/data/player/"
     comb_dir=f"{cur_dir}{src_dir}{player_name}"
+    print(player_name)
+    print(cur_dir)
+    print(src_dir)
+    print(comb_dir)
     if os.path.exists(comb_dir):
         file_name=str(input("Do you Want to Play Saved Game?(y/n): "))
         if file_name == "y":
@@ -19,17 +23,41 @@ def cave_file_func():
                     player_name_num=int(player_name_str) if player_name_str.isdigit() else 0
                     if player_name_num > player_name_num_max:
                         player_name_num_max=player_name_num
+
+                        # add else statement where it will create a new player file "player1"
                 else:
                     print(f"{player_name} Folder is Corrupted!") # If a player folder "messi" contains a .txt file with another player's name "ronaldo1.txt"
             player_name_file=f"{player_name}{player_name_num_max}.txt"
             player_name_file_dir=os.path.join(comb_dir,player_name_file)
             with open (player_name_file_dir, "a") as a:
+
+                # introduce keeping track of how many times this file was opened
+
                 a.write("Continuing Previous Exploration...\n")
         elif file_name == "n":
-            ""
+            player_name_num_max=0
+            for i_file in os.listdir(comb_dir):
+                if i_file.endswith(".txt") and player_name in i_file.lower():
+                    player_name_seq=re.search(rf"{re.escape(player_name)}(\d*)\.txt", i_file.lower())
+                    player_name_str=player_name_seq.group(1)
+                    player_name_num=int(player_name_str) if player_name_str.isdigit() else 0
+                    if player_name_num > player_name_num_max:
+                        player_name_num_max=player_name_num
+
+                         # add else statement where it will create a new player file "player1"
+                else:
+                    print(f"{player_name} Folder is Corrupted!") # If a player folder "messi" contains a .txt file with another player's name "ronaldo1.txt"
+            player_name_file=f"{player_name}{player_name_num_max+1}.txt"
+            player_name_file_dir=os.path.join(comb_dir,player_name_file)
+            with open (player_name_file_dir, "w") as w:
+                w.write("You have Started Your First Cave Exploration.\n")
         else:
-            ""
+            print("Please Enter Valid Characters (y/n).")
     else:
-        ""
+        os.makedirs(comb_dir)
+        player_name_file=f"{player_name}1.txt"
+        comb_file_dir=os.path.join(comb_dir,player_name_file)
+        with open (comb_file_dir, "w") as w:
+            w.write("You have Started Your First Cave Exploration.\n")
 
 cave_file_func()
